e7aa0980768997383e1f521ff7ce9f762269f7bb
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2019-11-07 17:16:30 +0100

    Implement a `replace cmd`
    The --kakoune option is here so that you don't have to do it in kak script :P

--- kak_spell/main.py
+++ kak_spell/main.py
@@ -46,6 +46,29 @@ def check(path, *, lang):
                 print(f"{path}:{lineno}:{error.wordpos+1}: error: {error.word}")
 
 
+def kak_menu_from_suggestions(suggestions):
+    menu = ""
+    for entry in suggestions:
+        # Note: %{...} is kakoune way of grouping stuff that may - or not
+        # contains quotes, which prevents us from using `%`, `.format()` or
+        # f-strings :P
+        menu_entry = "%{ENTRY} %{execute-keys -itersel %{cENTRY<esc>be}}"
+        menu_entry = menu_entry.replace("ENTRY", entry)
+        menu += " " + menu_entry
+    return "menu " + menu
+
+
+def replace(word, *, lang, kak_output):
+    pwl_path = get_pwl_path(lang)
+    dict_with_pwl = enchant.DictWithPWL(lang, str(pwl_path))
+    suggestions = dict_with_pwl.suggest(word)
+    if kak_output:
+        menu = kak_menu_from_suggestions(suggestions)
+        print(menu)
+    else:
+        print(" ".join(suggestions))
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--lang", default="en_US")
@@ -61,6 +84,12 @@ def main():
     remove_parser = subparsers.add_parser("remove")
     remove_parser.add_argument("word")
 
+    replace_parser = subparsers.add_parser("replace")
+    replace_parser.add_argument(
+        "--kakoune", action="store_true", help="Output kak-script compatible output"
+    )
+    replace_parser.add_argument("word")
+
     args = parser.parse_args()
     lang = args.lang
 
@@ -73,6 +102,10 @@ def main():
     elif args.command == "check":
         path = args.path
         check(path, lang=lang)
+    elif args.command == "replace":
+        word = args.word
+        kakoune = args.kakoune
+        replace(word, lang=lang, kak_output=kakoune)
     else:
         parser.print_help()
         sys.exit(1)
