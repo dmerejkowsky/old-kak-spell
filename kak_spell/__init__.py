6459c343c8e49b9007cb33020f2c44f51dd32746
Dimitri Merejkowsky <d.merej@gmail.com>
2020-04-12 21:51:06 +0200

    Always log
    We'll remove this function when we'll be done debugging

--- kak_spell/__init__.py
+++ kak_spell/__init__.py
@@ -5,7 +5,5 @@ import datetime
 
 # Useful when debugging kak scripts
 def log(stuff: Any) -> None:
-    if not os.environ.get("KAK_SPELL_DEBUG"):
-        return
     with open("/tmp/kak-spell.log", "a") as f:
         f.write(str(datetime.datetime.now()) + " " + str(stuff) + "\n")
